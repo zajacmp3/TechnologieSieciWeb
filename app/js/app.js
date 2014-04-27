c8e1d2dd84b5b2eea00eacda86e39703e66d28bc
zajacmp3 <deejay.zajac@gmail.com>
2014-04-27 14:51:02 +0200

    Poprawa routera

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'MyCtrl'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'MyCtrl'})
+    .when('/dupa', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl'})
+    .otherwise({redirectTo: '/view1'});
 }]);
